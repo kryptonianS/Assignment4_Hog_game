ad01d0bd5d6ba879f8960b7a2a0f1e64b7a40505
clin <clin@ccsf.edu>
2017-10-07 08:58:27 -0700

    Updated Round.java
    Updated Round.java with a toString function. Also update Test_round.java to display new output.
    
    Added some comments to round.java

--- Round.java
+++ Round.java
@@ -1,51 +1,53 @@
-//********************************************************************
-//  Round.java       
-//
+/**
+  * Round.java class to represent round of a game.  
+  * accessors include getPlayer, getRoundSCore, getPotentialTotal,  getStopInd.
+  * write methods include addRoundScore, setStopInd
+  * tostring method returns results of current round.
+  *  @author(s) Chien Lin, Lena Zheng, Qd Li
+  *  @version 1.0c
+  *  @since 2017-10-07 
 //  Represents the round of rolling the pair of dies
-//********************************************************************
-
+*/
 public class Round
 {
     // int totalScore;
     String currentPlayer;
-    int roundScore = 0;
+    int totalScore, roundScore = 0;
     boolean stopInd = false;
-
-
 
     //-----------------------------------------------------------------
     //  Constructor: for each round of the game
     //-----------------------------------------------------------------
-    public Round(String player)
-    {
-        currentPlayer = player;
-    }
-
-    //-----------------------------------------------------------------
-    //  Rolls the die and returns the result.
-    //-----------------------------------------------------------------
-    // public void roll()
-    // {
-    //     onePair.rollDice();
-    //     System.out.println(onePair.toString());
-    // }
+    public Round(String player, int inTotalScore)
+        {
+            currentPlayer = player;
+            totalScore = inTotalScore;
+        }
 
     public String getPlayer()
         {return currentPlayer;}
 
     public void addRoundScore(int sumDice)
-        {roundScore += sumDice;}
+        {roundScore += sumDice;
+		 totalScore += sumDice;
+		}
 
     public int getRoundScore()
         {return roundScore;}
 
-    public int getPotentialTotal(int currentTotal)
-        {return roundScore + currentTotal;}
+    public int getPotentialTotal()
+        {return totalScore;}
 
     public boolean getStopInd()
         {return stopInd;}
 
     public void setStopInd(boolean indicator)
         {stopInd = indicator;}
-
+		
+	// Returns string printing potential_total and round_total  
+  public String toString() 
+     {String result = "Cuurent Round: " + roundScore + "\n" + 
+	                  "Potential Total: " + totalScore; 
+	 return result;}
+   
 }
--- Test_round.java
+++ Test_round.java
@@ -14,7 +14,7 @@
   *    b) set roundTotal and potentialTotal to 0.
   *    c)**** until die1 = 1 or die2 = 1 ***********
   *        i) roll a pair of die
-  *          ii) if die1 <> 1 and die2 <> 1 then do next steps.
+           ii) if die1 <> 1 and die2 <> 1 then do next steps.
   *            1) sum result into roundTotal
   *            2) add roundTotal to potentialTotal
   *            3) print roundTotal and potentialTotal
@@ -28,40 +28,38 @@
   *********  end c  ************************************************************
   ***  end II  *****************************************************************
   * III) output current player as winner and print out final score.
- *
+
  */
 
 public class Test_round {
-    public static void main(String[] args){
-      // int cur_round = 2;
-      int humanCurrentTotalScore = 0;
+   public static void main(String[] args){
+      int currRound = 2;
+	  String my_player; 
+
 // TESTING current classes.
-      PairOfDie DiePair = new PairOfDie();
+      my_player = ((currRound%2 == 1) ? "Computer" : "Human");
+	  PairOfDie DiePair = new PairOfDie();
       //should assign player based on round we are in. odd round is computer even round is human
-	  // Round  myRound = new Round(cur_round);
-      Round  myRound = new Round("Human");
-	  // myRound.setPotentialTotal(1000);
+	  Round  myRound = new Round(my_player, 1000);
 	  
       System.out.println("Current Player: " + myRound.getPlayer());
+
       System.out.println("Current Score: " + myRound.getRoundScore());
 
       DiePair.rollDice();
-      System.out.println("\nFirst Dice: " + DiePair.getDie1());
-      System.out.println("Second Dice: " + DiePair.getDie2());
+      System.out.println(DiePair.toString());
       System.out.println("Sum of Dice: " + DiePair.sumDice());
 
-	  // //add round score should add to both round score and potential total.
+	  //add round score should add to both round score and potential total.
       myRound.addRoundScore(DiePair.sumDice()); 
       	  
-      System.out.println("Current Round Score: " + myRound.getRoundScore());
-	  System.out.println("Current Potential Total Score: " + myRound.getPotentialTotal(humanCurrentTotalScore));
-
+      System.out.println(myRound.toString());
 	  
-   //    System.out.println("Stop Indicator: " + myRound.getStopInd());
-   //    myRound.setStopInd(true);
-   //    System.out.println("Stop Indicator: " + myRound.getStopInd());
+      System.out.println("Stop Indicator: " + myRound.getStopInd());
+      myRound.setStopInd(true);
+      System.out.println("Stop Indicator: " + myRound.getStopInd());
 
 
-    }
+   }
 
 }
